514314c1b2f1241fe86de5026d0980721016e441
Arnd Bergmann <arnd@arndb.de>
2026-09-16 10:34:46 +0200

    mm/vmscan: avoid false-positive -Wuninitialized warning, again
    I previously worked around a false-postive gcc-16 warning in the
    get_tier_idx() function, by adding a fake initializer. This happens with
    the -fsanitize=bounds sanitizer when the compiler creates a specialized
    variant of isolate_folios():
    
    In function 'get_tier_idx',
        inlined from 'isolate_folios.constprop' at mm/vmscan.c:4982:9:
    mm/vmscan.c:4934:9: error: 'sp.refaulted' is used uninitialized [-Werror=uninitialized]
     4934 |         read_ctrl_pos(lruvec, type, LRU_TIER_MIN, LRU_TIER_MIN, 2, &sp);
          |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    mm/vmscan.c: In function 'isolate_folios.constprop':
    mm/vmscan.c:4946:25: note: 'sp.refaulted' was declared here
     4946 |         struct ctrl_pos sp, pv = {};
          |                         ^~
    
    Adding another "= {}" would solve the problem as well, but to prevent
    this from happening again after the next code refactoring, try instead to
    prevent this by forbidding interprocedural optimizations on this function.
    
    Link: https://lore.kernel.org/all/20260213123902.3466040-1-arnd@kernel.org/
    Link: https://lore.kernel.org/20260916083456.4136132-1-arnd@kernel.org
    Fixes: 3de705a43a46 ("mm/vmscan: avoid false-positive -Wuninitialized warning")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Baoquan He <baoquan.he@linux.dev>
    Cc: Barry Song <baohua@kernel.org>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Kairui Song <kasong@tencent.com>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Wei Xu <weixugc@google.com>
    Cc: Yuanchu Xie <yuanchu@google.com>
    Cc: <stable@vger.kernel.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3220,8 +3220,12 @@ struct ctrl_pos {
 	int gain;
 };
 
-static void read_ctrl_pos(struct lruvec *lruvec, int type, int tier, int gain,
-			  struct ctrl_pos *pos)
+/*
+ * __noipa works around  gcc-16 warning for uninitialized use of
+ * pos->refaulted
+ */
+static __noipa void read_ctrl_pos(struct lruvec *lruvec, int type, int tier,
+			  int gain, struct ctrl_pos *pos)
 {
 	int i;
 	struct lru_gen_folio *lrugen = &lruvec->lrugen;
@@ -4871,7 +4875,7 @@ static int scan_folios(unsigned long nr_to_scan, struct lruvec *lruvec,
 static int get_tier_idx(struct lruvec *lruvec, int type)
 {
 	int tier;
-	struct ctrl_pos sp, pv = {};
+	struct ctrl_pos sp, pv;
 
 	/*
 	 * To leave a margin for fluctuations, use a larger gain factor (2:3).
@@ -4890,7 +4894,7 @@ static int get_tier_idx(struct lruvec *lruvec, int type)
 
 static int get_type_to_scan(struct lruvec *lruvec, int swappiness)
 {
-	struct ctrl_pos sp, pv = {};
+	struct ctrl_pos sp, pv;
 
 	if (swappiness <= MIN_SWAPPINESS + 1)
 		return LRU_GEN_FILE;
